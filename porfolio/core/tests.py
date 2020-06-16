78d00af3d6900a9e1cb6f0397801384d92d84504
kevin-kili <kilikevin435@gmail.com>
2020-06-16 07:27:49 +0300

    added the bootstrap libraries

--- porfolio/core/tests.py
+++ porfolio/core/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import unittest
+from .models import Project
 
 # Create your tests here.
+
+class TestProject(unittest.TestCase):
+    """
+    Class to test the behaviours of the Project model.
+    """
+
+    def setUp(self):
+        self.new_project = Project('AIF Website', 'This is a site for the Agricultural Industry Forum', 'http://aif.co.ke')
+
+    def tearDown(self):
+        Project.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_project, Project))
