531f3b492fc29084fb1ddb7a9c44ad533e6ab2fb
kevin-kili <kilikevin435@gmail.com>
2020-06-23 11:50:09 +0300

    deployment to heroku

--- porfolio/porfolio/settings.py
+++ porfolio/porfolio/settings.py
@@ -129,11 +129,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+STATICFILES_DIRS = (Usisahau nikucall ukiwa karibu 
     os.path.join(BASE_DIR, 'static'),
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
-django_heroku.settings(locals()
+# django_heroku.settings(locals()
